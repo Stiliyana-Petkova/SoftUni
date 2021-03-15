9acc9c6180d70a321fd494cdcdcad14d5ff9470f
Stiliyana Petkova <76534910+Stiliyana-Petkova@users.noreply.github.com>
2021-03-15 11:01:27 +0200

    Add files via upload

---
+++ AssociativeArrays/phoneBook.js
@@ -0,0 +1,17 @@
+function phonebook(input) {
+
+    let phoneBook = {};
+
+    for (const line of input) {
+        let [name,phone] = line.split(' ');
+        phoneBook[name] = phone;
+    }
+    for (const key in phoneBook) {
+        console.log(`${key} -> ${phoneBook[key]}`);
+    }
+}
+
+phonebook(['Tim 0834212554',
+'Peter 0877547887',
+'Bill 0896543112',
+'Tim 0876566344'])
